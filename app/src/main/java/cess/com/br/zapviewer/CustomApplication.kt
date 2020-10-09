0d666d57e46a4a773108608d4b752e396b97da67
Carlos Eduardo Simoes <carlos.simoes@terceiro-sky.com.br>
2020-10-09 11:18:13 -0300

    Adds koin setup to CustomApplication

--- app/src/main/java/cess/com/br/zapviewer/CustomApplication.kt
+++ app/src/main/java/cess/com/br/zapviewer/CustomApplication.kt
@@ -1,9 +1,24 @@
 package cess.com.br.zapviewer
 
 import android.app.Application
+import cess.com.br.zapviewer.di.networkModule
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.context.startKoin
 
 open class CustomApplication: Application() {
     override fun onCreate() {
         super.onCreate()
+        setupKoin()
+    }
+
+    protected open fun setupKoin() {
+        startKoin {
+            modules(
+                listOf(
+                    networkModule
+                )
+            )
+            androidContext(this@CustomApplication)
+        }
     }
 }
